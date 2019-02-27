d3ff966c3e62322837fa46fe8c47c52682364b4f
github username <github email address>
2019-02-26 23:04:40 -0500

    review controller pt 1

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::ReviewsController < ApplicationController
   def create
+    @review = Review.new(review_params)
+    if @review.save
+      render json: @review
+    else
+      render json: {errors: @review.errors.full_messages}
+    end
   end
 
-  def update
+  # def update
+  # end
+
+private
+
+  def review_params
+    params.require(:review).permit(:user_id, :establishment_id, :YelpID, :review_text, :poc_rating, :women_rating, :lgbtq_rating)
   end
 end
