ebaa4abab01593e3935ca05397336bb9470d946d
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-03-31 16:53:34 -0400

    user login validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :reviews
   has_secure_password
   validates :username, uniqueness: { case_sensitive: false }
+  validates :username, length: { minimum: 2 }
+
 end
