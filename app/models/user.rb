412073299ae994f517537243ce5f2bf23a63036a
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-04-02 21:10:05 -0400

    user updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, uniqueness: { case_sensitive: false }
   validates :username, length: { minimum: 2 }
-
+  validates :username, :first_name, :password, presence: true
 end
