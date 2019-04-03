a045368183b3de438d67b111c01be3aa2cff8c37
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-04-03 01:16:46 -0400

    changed to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_25_232809) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "establishments", force: :cascade do |t|
     t.string "yelp_id"
     t.string "name"
